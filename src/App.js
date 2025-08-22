0c2dc15cf42c157f946768b360a8bee7c2ea5ff7
Mikaal <mikaalkhan99@gmail.com>
2025-08-22 12:43:23 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
+import logo from './spinningpic.ico';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    var displayJoke = function() {
+      fetch("https://official-joke-api.appspot.com/random_joke")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        var joke = document.getElementById("joke-display");
+        var mainJoke = data.setup;
+        console.log(mainJoke);
+        var punchline = data.punchline;
+        console.log(punchline);
+        joke.textContent = "The random joke is : " + mainJoke + "..." + punchline;
+      })
+    }
+    var button = document.getElementById("generate");
+    button.addEventListener("click", displayJoke);
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1 id="title">RANDOM JOKE GENERATOR!!!!!!!</h1>
+        <button id="generate">GENERATE JOKE</button>
+        <p id="joke-display">
+          The random joke is :
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hope you had fun!!! Visit my <a href="https://github.com/MikFlurry1" id="link">Github</a></p>
       </header>
     </div>
   );
