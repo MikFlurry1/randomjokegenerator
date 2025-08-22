b00f483023687256bcb079985b11afb1ca826121
Mikaal <mikaalkhan99@gmail.com>
2025-08-22 13:25:58 +0400

    Updates to game

--- src/App.js
+++ src/App.js
@@ -3,31 +3,25 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  useEffect(() => {
-    var displayJoke = function() {
-      fetch("https://official-joke-api.appspot.com/random_joke")
+  const [joke, setJoke] = useState("");
+
+  const displayJoke = () => {
+    setJoke("Loading....")
+    fetch("https://official-joke-api.appspot.com/random_joke")
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        var joke = document.getElementById("joke-display");
-        var mainJoke = data.setup;
-        console.log(mainJoke);
-        var punchline = data.punchline;
-        console.log(punchline);
-        joke.textContent = "The random joke is : " + mainJoke + "..." + punchline;
+        setJoke(data.setup + "..." + data.punchline);
       })
-    }
-    var button = document.getElementById("generate");
-    button.addEventListener("click", displayJoke);
-  }, []);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 id="title">RANDOM JOKE GENERATOR!!!!!!!</h1>
-        <button id="generate">GENERATE JOKE</button>
+        <button id="generate" onClick={displayJoke}>GENERATE JOKE</button>
         <p id="joke-display">
-          The random joke is :
+          The random joke is : {joke}
         </p>
         <p>Hope you had fun!!! Visit my <a href="https://github.com/MikFlurry1" id="link">Github</a></p>
       </header>
